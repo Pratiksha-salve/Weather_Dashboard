b75e4daebcbcac13b088cd341da140f7a1376106
Pratiksha-salve <pratikshasalve1026@gmail.com>
2024-10-19 20:21:10 +0530

    added current location button and worked on ui

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const search = document.getElementById('search-btn')
 
 search.addEventListener('click', searching)
 
-function searching(){
+function searching() {
     const city = document.getElementById('city-input').value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             const localWeather = document.getElementById('local');
-            localWeather.innerHTML = `<h1>${data.name}</h1>
-                <p>Temperature: ${data.main.temp - 273.15}°C</p>
-                <p>Wind: ${visibility.wind.speed}m/s</p>
+            localWeather.innerHTML = `<h1 class="text-2xl font-bold">${data.name}</h1>
+                <p>Temperature: ${(data.main.temp - 273.15).toFixed(2)}°C</p>
+                <p>Wind: ${data.wind.speed} m/s</p>
                 <p>Humidity: ${data.main.humidity}%</p>
-                <p>${weather.description}</p>`;
+                <p>Condition: ${data.weather[0].description}</p>`;
         })
         .catch(error => console.log('Error:', error));
-};
+}
